fb260dce8a8724a501d8ee92c9e62d6b43ec0215
nameT <namet117@163.com>
2018-06-04 20:25:37 +0800

    update goods table

--- database/migrations/2018_06_03_102247_create_goods_table.php
+++ database/migrations/2018_06_03_102247_create_goods_table.php
@@ -20,11 +20,17 @@ class CreateGoodsTable extends Migration
             $table->integer('cate_id')->default(0)->comment('主品类ID');
             $table->integer('type_id')->default(0)->comment('类型ID');
             $table->string('name')->comment('名称');
+            $table->string('description')->nullable()->comment('描述');
             $table->integer('stock')->default(0)->comment('库存');
             $table->integer('warn_stock')->default(0)->comment('警告库存');
-            $table->string('description')->nullable()->comment('描述');
-
+            $table->integer('order')->index()->comment('排序');
+            $table->decmial('price', 10, 2)->default(0)->comment('销售价');
+            $table->decmial('market_price', 10, 2)->default(0)->comment('市场价');
+            $table->decimal('cost_price', 10, 2)->default(0)->comment('成本价');
+            $table->decmial('weight', 6, 3)->default(0)->comment('重量');
             $table->timestamps();
+            $table->timestamp('up_at')->nullable()->comment('上架时间');
+            $table->timestamp('down_at')->nullable()->comment('下架时间');
             $table->softDeletes();
         });
     }
