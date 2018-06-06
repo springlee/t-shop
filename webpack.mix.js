49a930907a7b92ecc81268d311d1f7733c8c46bf
name-T <namet117@163.com>
2018-06-06 22:43:05 +0800

    updated bootstrap and laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,4 @@ mix.disableNotifications();
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/admin.scss', 'public/css')
-    .js('resources/assets/js/admin.js', 'public/js')
     .version();
